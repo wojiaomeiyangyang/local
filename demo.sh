4ede09d9677c70a0a5118bffa16164313a89388c
meiyangyang <914096192@qq.com>
2018-09-03 20:34:53 +0800

    脚本作业

---
+++ demo.sh
@@ -0,0 +1,15 @@
+if [ -d $1 ]; then
+exit
+else
+mkdir $1
+cd $1
+mkdir css js
+touch index.html css/style.css js/main.js
+echo '<!DOCTYPE>'>>index.html
+echo '<title>Hello</title>'>>index.html
+echo '<h1>Hi</h1>'>>index.html
+echo 'h1{color: red;}'>>css/style.css
+echo 'var string = "Hello World"'>>js/main.js
+echo 'var string = "alert(string)"'>>js/main.js
+exit
+fi
